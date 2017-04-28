e72ffd1d6a7da3e4c10dd2c04739a9a2e2ab2fb9
Victor Aguilar <victor.aguilar@entrepids.com>
2017-04-27 19:18:58 -0500

    Method to save Gift entity

--- magento/app/code/local/Entrepids/Giftregistry/Model/Entity.php
+++ magento/app/code/local/Entrepids/Giftregistry/Model/Entity.php
@@ -6,4 +6,21 @@ class Entrepids_Giftregistry_Model_Entity extends Mage_Core_Model_Abstract
         $this->_init('entrepids_giftregistry/entity');
         parent::_construct();
     }
+
+    public function updateRegistryData(Mage_Customer_Model_Customer $customer, $data)
+    {
+        if (empty($data)) {
+            Mage::log("Error Processing Request: Insufficient Data Provided", Zend_Log::ERR);
+        }
+
+        $this->setCustomerId($customer->getId());
+        $this->setWebsiteId($customer->getWebsiteId());
+        $this->setTypeId($data['type_id']);
+        $this->setEventName($data['event_name']);
+        $this->setEventDate($data['event_date']);
+        $this->setEventCountry($data['event_country']);
+        $this->setEventLocation($data['event_location']);
+
+        return $this;
+    }
 }
